a429621000f272e5d6a3031afe35f80ff66731f8
bmcniff <42455310+bmcniff@users.noreply.github.com>
2020-05-25 12:59:15 -0400

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,22 @@
 def players_by_country_and_position(squads_list):
-    pass
+    final = {}
+    result = {}
+    for player in squads_list:
+        player_dict = {}
+        player_dict['number'] = player[0]
+        player_dict['position'] = player[1]
+        player_dict['name'] = player[2]
+        player_dict['date_of_birth'] = player[3]
+        player_dict['caps'] = player[4]
+        player_dict['club'] = player[5]
+        player_dict['country'] = player[6]
+        player_dict['club_country'] = player[7]
+        player_dict['year'] = player[8]
+        position = player[1]
+        result.setdefault(position, [])
+        result[position].append(player_dict)
+    for position, player in result.items():
+        country = player[0]['country']
+        final.setdefault(country, {})
+        final[country].update({position : player})
+    return final
