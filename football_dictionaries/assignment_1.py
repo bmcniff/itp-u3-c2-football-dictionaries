334dc408298b6c8762292f0eba83072edd0febbf
bmcniff <42455310+bmcniff@users.noreply.github.com>
2020-05-25 11:35:17 -0400

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,15 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    for i in squads_list:
+        player = {}
+        player['number'] = i[0]
+        player['position'] = i[1]
+        player['name'] = i[2]
+        player['date_of_birth'] = i[3]
+        player['caps'] = i[4]
+        player['club'] = i[5]
+        player['country'] = i[6]
+        player['club_country'] = i[7]
+        player['year'] = i[8]
+        result.append(player)
+    return result
